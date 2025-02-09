4c137d050bc30a760426eb0cac9482e6c2d4c1cb
MallidiSasiKiranReddy <kiransasi401@gmail.com>
2025-02-09 19:47:38 +0530

    Initial commit

--- carrercompass-master/main.py
+++ carrercompass-master/main.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import pandas as pd
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 path = "C:/Users/kiran/Downloads/chromedriver-win64/chromedriver-win64/chromedriver.exe"
 chrome_options = Options()
@@ -39,37 +41,76 @@ location.send_keys("Hyderabad")
 location.send_keys(Keys.ENTER)
 time.sleep(5)
 
-tot_jobs = driver.find_element(By.XPATH,'//*[@id="left-column"]/div[1]/h1').text
+for _ in range(4):
+    findmore = driver.find_element(By.XPATH, '// *[ @ id = "left-column"] / div[2] / div / div / button')
+    findmore.send_keys(Keys.ENTER)
+    time.sleep(4)
+
+wait = WebDriverWait(driver, 10)
+tot_jobs = driver.find_element(By.XPATH,'//*[@id="left-column"]/div[1]/span/div/h1').text
 
 job_list = []
 all_jobs = driver.find_elements(By.CLASS_NAME,'JobCard_jobCardContainer__arQlW')
 time.sleep(5)
+
 for job in all_jobs:
     try:
+        job.click()
+        time.sleep(1)
         company_name = job.find_element(By.CLASS_NAME, 'EmployerProfile_compactEmployerName__9MGcV').text
+        try:
+            salary_estimate = job.find_element(By.CLASS_NAME, 'JobCard_salaryEstimate__QpbTW').text
+        except Exception:
+            salary_estimate = "N/A"
         try:
             rating = job.find_element(By.CLASS_NAME, 'rating-single-star_RatingText__XENmU').text
         except Exception:
             rating = "N/A"  # Assign "N/A" if rating is missing
-        # rating = job.find_element(By.CLASS_NAME, 'rating-single-star_RatingText__XENmU').text
         role_name = job.find_element(By.CLASS_NAME, 'JobCard_jobTitle__GLyJ1').text
-        print(company_name, role_name, rating)
+
+        try:
+            recommend_percent = WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'JobDetails_employerStatsDonuts__uWTLY'))
+            ).text
+        except Exception:
+            recommend_percent = "N/A"
+        try:
+            pros_text = job.find_element(By.XPATH, '//*[@id="app-navigation"]/div[4]/div[2]/div[2]/div/div[1]/section/section[5]/div/div[1]/ul').text
+        except:
+            pros_text = "No Pros Available"
+
+        try:
+            cons_text = job.find_element(By.XPATH, '//*[@id="app-navigation"]/div[4]/div[2]/div[2]/div/div[1]/section/section[5]/div/div[2]/ul').text
+        except:
+            cons_text = "No Cons Available"
+        print(company_name, role_name, rating, salary_estimate)
+        print("\n")
+        print(recommend_percent)
+        print("\n")
+        print(pros_text)
+        print("\n")
+        print(cons_text)
         job_list.append({
             'Company': company_name,
             'Rating': rating,
-            'Role_name': role_name
+            'Role_name': role_name,
+            'Salary_Estimate': salary_estimate,
+            'Pros': pros_text,
+            'Cons': cons_text,
+            'Recommend %': recommend_percent
         })
     except Exception as e:
-        # Handle missing elements or other exceptions
-        print(f"Error fetching details for a job card: {e}")
+        print(f"Error fetching details for a job card: ")
 
+print("\n")
+print(tot_jobs)
 print(job_list)
 df=pd.DataFrame(job_list)
-print(df)
-# file_path = 'C:/Users/kiran/Documents/data.xlsx'
-# df.to_excel(file_path,index=False)
-# print(f"DataFrame has been saved to {file_path}")
-# EmployerProfile_employerNameContainer__ptolz
+df.to_csv("glassdors_jobs.csv",index=False,encoding='utf-8')
+print("Data Saved to glassdoors_jobs.csv successfully")
+
+
+
 
 
 
